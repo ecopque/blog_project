4900cae41322c6a0b689b737c210de76fb22cd64
Edson Copque <ecop@disroot.org>
2024-08-30 17:10:28 -0300

    Create.

---
+++ scripts/commands.sh
@@ -0,0 +1,16 @@
+# /blog_project/scripts/commands.sh
+
+#!/bin/sh
+
+set -e #14: ##
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do ##
+    echo "Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..." ##
+    sleep 0.1 ##
+done ##
+
+echo "Postgres Database Started Successfully ($POSTGRES_HOST $POSTGRES_PORT) ..." ##
+
+python3 manage.py collectstatic ##
+python3 manage.py migrate
+python3 manage.py runserver
