c6569b8e995607ff3c7cfe3b9e212578db9c53d7
Edson Copque <ecop@disroot.org>
2024-08-30 17:50:24 -0300

    Cleanup code.

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -2,15 +2,15 @@
 
 #!/bin/sh
 
-set -e #14: ##
+set -e #14:
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do ##
-    echo "Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..." ##
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    echo "Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
     sleep 0.1 ##
 done ##
 
-echo "Postgres Database Started Successfully ($POSTGRES_HOST $POSTGRES_PORT) ..." ##
+echo "Postgres Database Started Successfully ($POSTGRES_HOST $POSTGRES_PORT) ..."
 
-python3 manage.py collectstatic ##
+python3 manage.py collectstatic
 python3 manage.py migrate
 python3 manage.py runserver
