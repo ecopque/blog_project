e242cdcd97150fe5b26feae43d3a4e1b13eab41b
Edson Copque <ecop@disroot.org>
2024-09-02 14:41:07 -0300

    Code cleanup

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -11,7 +11,7 @@ done
 
 echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST $POSTGRES_PORT) ..."
 
-python3 manage.py collectstatic --noinput ##
-python3 manage.py makemigrations --noinput ##
-python3 manage.py migrate --noinput ##
-python3 manage.py runserver 0.0.0.0:8000 ##
+python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 manage.py runserver 0.0.0.0:8000
