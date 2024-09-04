ee791837df0737f15fe4667dae9dfc97c88007e8
Edson Copque <ecop@disroot.org>
2024-09-04 16:10:34 -0300

    Under construction.

---
+++ djangoapp/blog/urls.py
@@ -0,0 +1,10 @@
+# /blog_project/djangoapp/blog/urls.py
+
+from django.urls import path
+from blog.views import index # /blog_project/djangoapp/blog/views.py
+
+
+app_name = 'blog'
+urlpatterns = [
+    path('', index, name='index'),
+]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -3,4 +3,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'blog/index.html')
+    return render(request, 'blog/index.html') ##
