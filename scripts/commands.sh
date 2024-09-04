cb91095df83e6b157e5f492f6aaa52baf25f6436
Edson Copque <ecop@disroot.org>
2024-09-04 10:56:19 -0300

    Comments add

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -4,8 +4,8 @@
 
 set -e
 
-wait_psql.sh
-makemigrations.sh
-migrate.sh
-collectstatic.sh
-runserver.sh
+wait_psql.sh # /blog_project/scripts/wait_psql.sh
+makemigrations.sh # /blog_project/scripts/makemigrations.sh
+migrate.sh # /blog_project/scripts/migrate.sh
+collectstatic.sh # /blog_project/scripts/collectstatic.sh
+runserver.sh # /blog_project/scripts/runserver.sh
