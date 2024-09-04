b8df151f5401fee70d823d054aa55cea8bc66790
Edson Copque <ecop@disroot.org>
2024-09-04 17:59:40 -0300

    Starting...

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog', ## # /blog_project/djangoapp/blog/apps.py
+    'blog', # /blog_project/djangoapp/blog/apps.py
 ]
 
 MIDDLEWARE = [
