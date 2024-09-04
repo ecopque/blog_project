186ed04cfd4d6f3bbf68cde0704461de929251a4
Edson Copque <ecop@disroot.org>
2024-09-04 10:31:57 -0300

    Testing

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -2,14 +2,7 @@
 
 set -e
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-    echo "⏳ Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
-    sleep 2
-done
-
-echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST $POSTGRES_PORT) ..."
-
-
+wait_psql.sh
 makemigrations.sh
 migrate.sh
 collectstatic.sh
