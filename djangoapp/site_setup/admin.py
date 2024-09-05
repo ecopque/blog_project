2ae7983ae3d6df77a03eef0fe4896dbb39acebfe
Edson Copque <ecop@disroot.org>
2024-09-05 16:01:22 -0300

    Notes.

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -6,5 +6,5 @@ from site_setup.models import MenuLink
 @admin.register(MenuLink)
 class MenuLinkAdmin(admin.ModelAdmin):
     list_display = 'id', 'text', 'url_or_path', #6: ## # /blog_project/djangoapp/site_setup/models.py
-    list_display_links = 'id', 'text', 'url_or_path', ## # /blog_project/djangoapp/site_setup/models.py
-    search_fields = 'id', 'text', 'url_or_path', ## # /blog_project/djangoapp/site_setup/models.py
+    list_display_links = 'id', 'text', 'url_or_path', #7: ## # /blog_project/djangoapp/site_setup/models.py
+    search_fields = 'id', 'text', 'url_or_path', #8: ## # /blog_project/djangoapp/site_setup/models.py
