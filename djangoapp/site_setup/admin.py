80c279a05f0f6e74a2231f231b9d1397da76c1d1
Edson Copque <ecop@disroot.org>
2024-09-05 17:40:55 -0300

    Functions included.

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,7 +1,7 @@
 # /blog_project/djangoapp/site_setup/admin.py
 
 from django.contrib import admin
-from site_setup.models import MenuLink, SiteSetup ##
+from site_setup.models import MenuLink, SiteSetup
 
 @admin.register(MenuLink)
 class MenuLinkAdmin(admin.ModelAdmin):
@@ -9,9 +9,9 @@ class MenuLinkAdmin(admin.ModelAdmin):
     list_display_links = 'id', 'text', 'url_or_path', # /blog_project/djangoapp/site_setup/models.py
     search_fields = 'id', 'text', 'url_or_path', # /blog_project/djangoapp/site_setup/models.py
 
-@admin.register(SiteSetup) ##
-class SiteSetupAdmin(admin.ModelAdmin): ##
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
     list_display = 'title', 'description', # /blog_project/djangoapp/site_setup/models.py
 
-    def has_add_permission(self, request): ##
-        return not SiteSetup.objects.exists() #5: #6: ##
+    def has_add_permission(self, request):
+        return not SiteSetup.objects.exists() #5: #6:
--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -19,14 +19,14 @@ class SiteSetup(models.Model):
         verbose_name = 'Setup'
         verbose_name_plural = 'Setup'
 
-    title = models.CharField(max_length=65) #2: ##
-    description = models.CharField(max_length=255) ##
-    show_header = models.BooleanField(default=True) #3: ##
-    show_search = models.BooleanField(default=True) #3:
-    show_menu = models.BooleanField(default=True) #3:
-    show_description = models.BooleanField(default=True) #3:
-    show_pagination = models.BooleanField(default=True) #3:
-    show_footer = models.BooleanField(default=True) #3:
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=255)
+    show_header = models.BooleanField(default=True)
+    show_search = models.BooleanField(default=True)
+    show_menu = models.BooleanField(default=True)
+    show_description = models.BooleanField(default=True)
+    show_pagination = models.BooleanField(default=True)
+    show_footer = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
