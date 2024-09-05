c01e94f8921785736c387bdd4e5a443328ab1490
Edson Copque <ecop@disroot.org>
2024-09-05 15:55:39 -0300

    list_display add.

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+# /blog_project/djangoapp/site_setup/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from site_setup.models import MenuLink
+
+@admin.register(MenuLink)
+class MenuLinkAdmin(admin.ModelAdmin):
+    list_display = 'id', 'text', 'url_or_path', #6: ## # /blog_project/djangoapp/site_setup/models.py
