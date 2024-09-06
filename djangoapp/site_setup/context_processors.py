46624c2d3d442305f27b0013a2ad70d306db0310
Edson Copque <ecop@disroot.org>
2024-09-06 17:48:24 -0300

    Under construction.

--- djangoapp/site_setup/context_processors.py
+++ djangoapp/site_setup/context_processors.py
@@ -1,7 +1,7 @@
 # /blog_project/djangoapp/site_setup/context_processors.py
 
 def context_processor_example(request): ##
-    return {'example': '/blog_project/djangoapp/site_setup/context_processors.py'} ##
+    return {'example': 'EXAMPLE context_processors.py'} ##
 
 def site_setup(request): ##
-    return {'site_setup': '/blog_project/djangoapp/site_setup/context_processors.py'} ##
+    return {'site_setup': 'SITE_SETUP context_processors.py', 'title': 'Title'} ##
