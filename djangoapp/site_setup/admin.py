eff46b15fdd7986847337dc2c8cc4f58481795f0
Edson Copque <ecop@disroot.org>
2024-09-06 18:59:27 -0300

    Organizing code.

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,23 +1,34 @@
-# /blog_project/djangoapp/site_setup/admin.py
+# FILE: /blog_project/djangoapp/site_setup/admin.py
 
 from django.contrib import admin
 from site_setup.models import MenuLink, SiteSetup
 
 @admin.register(MenuLink)
 class MenuLinkAdmin(admin.ModelAdmin):
-    list_display = 'id', 'text', 'url_or_path', # /blog_project/djangoapp/site_setup/models.py
-    list_display_links = 'id', 'text', 'url_or_path', # /blog_project/djangoapp/site_setup/models.py
-    search_fields = 'id', 'text', 'url_or_path', # /blog_project/djangoapp/site_setup/models.py
+    
+    # LOCALE: # /blog_project/djangoapp/site_setup/models.py
+    list_display = 'id', 'text', 'url_or_path', 
+    list_display_links = 'id', 'text', 'url_or_path',
+    search_fields = 'id', 'text', 'url_or_path',
 
 class MenuLinkInline(admin.TabularInline):
-    model = MenuLink # /blog_project/djangoapp/site_setup/models.py
+    
+    # LOCALE: # /blog_project/djangoapp/site_setup/models.py
+    model = MenuLink
+    
     extra = 1
 
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
-    list_display = 'title', 'description', # /blog_project/djangoapp/site_setup/models.py
+    
+    # LOCALE: /blog_project/djangoapp/site_setup/models.py
+    list_display = 'title', 'description',
+    
     inlines = MenuLinkInline,
 
     def has_add_permission(self, request):
+
+        # COMMENT: Just one.
         # return not SiteSetup.objects.exists()
+
         return True
