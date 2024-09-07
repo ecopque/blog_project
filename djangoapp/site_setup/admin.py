d23ec2a6f5c578a85c67cf8d2d942efdfb0241bc
Edson Copque <ecop@disroot.org>
2024-09-07 19:08:01 -0300

    Under construction.

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -3,32 +3,42 @@
 from django.contrib import admin
 from site_setup.models import MenuLink, SiteSetup
 
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
+# IMPORT⬇: /blog_project/djangoapp/site_setup/models.py
+@admin.register(MenuLink) #1:
+class MenuLinkAdmin(admin.ModelAdmin): ##
     
-    # LOCALE: # /blog_project/djangoapp/site_setup/models.py
-    list_display = 'id', 'text', 'url_or_path', 
-    list_display_links = 'id', 'text', 'url_or_path',
-    search_fields = 'id', 'text', 'url_or_path',
+    # EXPORT⬇: values to /blog_project/djangoapp/site_setup/models.py
+    list_display = 'id', 'text', 'url_or_path', #2:
+    list_display_links = 'id', 'text', 'url_or_path', #2:
+    search_fields = 'id', 'text', 'url_or_path', #2:
 
-class MenuLinkInline(admin.TabularInline):
-    
-    # LOCALE: # /blog_project/djangoapp/site_setup/models.py
-    model = MenuLink
-    
-    extra = 1
+class MenuLinkInline(admin.TabularInline): #3: ##
+    # URL⬇: http://127.0.0.1:8000/admin/site_setup/sitesetup/add/
+    model = MenuLink #4: ##
+    extra = 3 #5:
 
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
     
-    # LOCALE: /blog_project/djangoapp/site_setup/models.py
+    # ???
     list_display = 'title', 'description',
     
     inlines = MenuLinkInline,
 
+    # ???
     def has_add_permission(self, request):
 
         # COMMENT: Just one.
         # return not SiteSetup.objects.exists()
 
+        # COMMENT: Vários.
         return True
+    
+
+# IMPORT, EXPORT and URL.
+
+#1: Registering 'MenuLink' in Django.
+#2: Fields displayed in Django Admin for the 'MenuLink' model defined in 'models.py'.
+#3: 'Inline' to add/edit 'MenuLink' directly in 'SiteSetup' in Django Admin.
+#4: Settings the 'MenuLink' template to be used as 'inline' in 'SiteSetup'.
+#5: Number of extra entries displayer in the inline interface when adding a new object. 
--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -2,59 +2,63 @@
 
 from django.db import models
 
-# LOCALE⬇: http://127.0.0.1:8000/admin/site_setup/menulink/
-class MenuLink(models.Model):
-    # COMMENT⬇: Set metadata options.
-    class Meta:
-        # LOCALE⬇: http://127.0.0.1:8000/admin/site_setup/menulink/
+# EXPORT⬇: /blog_project/djangoapp/site_setup/admin.py
+# URL⬇: http://127.0.0.1:8000/admin/site_setup/menulink/
+class MenuLink(models.Model): ##
+    class Meta: #2:
+        # URL⬇: http://127.0.0.1:8000/admin/site_setup/menulink/
         verbose_name = 'Menu Links'
         verbose_name_plural = 'Menu Links'
 
-    # LOCALE⬇: http://127.0.0.1:8000/admin/site_setup/menulink/add/
+    # URL⬇: http://127.0.0.1:8000/admin/site_setup/menulink/add/
+    # IMPORT⬇: values/var from /blog_project/djangoapp/site_setup/admin.py
     text = models.CharField(max_length=50)
     url_or_path = models.CharField(max_length=2048)
-    new_tab = models.BooleanField(default=False)
+    new_tab = models.BooleanField(default=False) #6:
 
-    # COMMENT1⬇: Before: class SiteSetup(models.Model).
-    # COMMENT2⬇: 'Sitesetup' can have multiple 'MenuLinks'. Otherwise it cannot.
-    # COMMENT3⬇: class SiteSetup(models.Model).
-    # POINT⬇: http://127.0.0.1:8000/admin/site_setup/sitesetup/add/
-    site_setup = models.ForeignKey('SiteSetup', on_delete=models.CASCADE, blank=True, null=True, default=None,)
+    # URL3⬇: http://127.0.0.1:8000/admin/site_setup/sitesetup/add/
+    site_setup = models.ForeignKey('SiteSetup', on_delete=models.CASCADE, blank=True, null=True, default=None,) #3: #4: ##
 
-    # COMMENT⬇: String representation.
-    def __str__(self):
+    def __str__(self): #7:
         return self.text
 
-# LOCALE⬇: http://127.0.0.1:8000/admin/site_setup/sitesetup/
-class SiteSetup(models.Model):
-    # COMMENT⬇: Set metadata options.
-    class Meta:
-        # LOCALE⬇: http://127.0.0.1:8000/admin/site_setup/sitesetup/
+# URL⬇: http://127.0.0.1:8000/admin/site_setup/sitesetup/
+# EXPORT⬇: /blog_project/djangoapp/site_setup/admin.py
+class SiteSetup(models.Model): ##
+    class Meta: #5:
+        # URL⬇: http://127.0.0.1:8000/admin/site_setup/sitesetup/
         verbose_name = 'Setup'
         verbose_name_plural = 'Setup'
     
-    # LOCALE⬇: http://127.0.0.1:8000/admin/site_setup/sitesetup/add/
-    # COMMENT⬇: Website title.
-    title = models.CharField(max_length=65)
-    # COMMENT⬇: Description field.
-    description = models.CharField(max_length=255)
+    # URL⬇: http://127.0.0.1:8000/admin/site_setup/sitesetup/add/
+    title = models.CharField(max_length=65) #8:
+    description = models.CharField(max_length=255) #9:
+    show_header = models.BooleanField(default=True) #10:
+    show_search = models.BooleanField(default=True) #10:
+    show_menu = models.BooleanField(default=True) #10:
+    show_description = models.BooleanField(default=True) #10:
+    show_pagination = models.BooleanField(default=True) #10:
+    show_footer = models.BooleanField(default=True) #10:
 
-    # COMMENT⬇: Visible fields and if selected marked.
-    # VISIBLE⬇: They will be visible on the website.
-    show_header = models.BooleanField(default=True)
-    show_search = models.BooleanField(default=True)
-    show_menu = models.BooleanField(default=True)
-    show_description = models.BooleanField(default=True)
-    show_pagination = models.BooleanField(default=True)
-    show_footer = models.BooleanField(default=True)
-
-    # COMMENT⬇: Website favicon image.
-    # LOCALE⬇: /blog_project/data/web/media/assets/favicon/"year"/"month"
-    favicon = models.ImageField(upload_to='assets/favicon/%Y/%m/', blank=True, default='') #1: ##
+    # URL⬇: http://127.0.0.1:8000/admin/site_setup/sitesetup/add/
+    # EXPORT⬇: /blog_project/data/web/media/assets/favicon/"year"/"month"
+    favicon = models.ImageField(upload_to='assets/favicon/%Y/%m/', blank=True, default='') #1: #11: ##
 
     def __str__(self):
         return self.title
     
     
 
+# IMPORT, EXPORT and URL.
+
 #1: Estamos definindo onde as imagens serão armazenadas e de que forma (na pasta teremos o ano e mês). Ela ainda poderá enviar sem a imagem e se for assim o valor padrão será uma string.
+#2: Set metadata options.
+#3: 'Sitesetup' can have multiple 'MenuLinks'. Otherwise it cannot.
+#4: Is you delete the link from 'site_setup', it will delete all links.
+#5: Set metadata options.
+#6: Django default box.
+#7: String representation.
+#8: Website title.
+#9: Description field.
+#10: Visible fields and if selected marked on Django Admin.
+#11: Website favicon image.
