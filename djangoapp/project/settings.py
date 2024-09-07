dcfca6ee78bd4d021fb43d9b74cb68e45273d02a
Edson Copque <ecop@disroot.org>
2024-09-07 10:39:12 -0300

    Insert comment.

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
 
                 # LOCALE: /blog_project/djangoapp/site_setup/context_processors.py
-                # Call the function.
+                # COMMENT: Call the function.
                 'site_setup.context_processors.context_processor_example',
 
                 # #LOCALE: /blog_project/djangoapp/site_setup/context_processors.py
