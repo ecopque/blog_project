a5b39b63d4bb2d5e4cd61dc99bd1b00846c5cbfe
Edson Copque <ecop@disroot.org>
2024-09-08 09:46:10 -0300

    Under construction.

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -17,22 +17,21 @@ class MenuLinkInline(admin.TabularInline): #3: ##
     model = MenuLink #4: ##
     extra = 3 #5:
 
+# IMPORT⬇: /blog_project/djangoapp/site_setup/models.py
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
     
-    # ???
-    list_display = 'title', 'description',
+    # URL⬇: http://127.0.0.1:8000/admin/site_setup/sitesetup/
+    # IMPORT⬇: /blog_project/djangoapp/site_setup/models.py
+    list_display = 'title', 'description', #6: ##
     
-    inlines = MenuLinkInline,
+    inlines = MenuLinkInline, ##
 
-    # ???
-    def has_add_permission(self, request):
+    #URL⬇: http://127.0.0.1:8000/admin/site_setup/sitesetup/add/
+    def has_add_permission(self, request): ##
 
-        # COMMENT: Just one.
-        # return not SiteSetup.objects.exists()
-
-        # COMMENT: Vários.
-        return True
+        # return not SiteSetup.objects.exists() #8:
+        return True #7:
     
 
 # IMPORT, EXPORT and URL.
@@ -41,4 +40,7 @@ class SiteSetupAdmin(admin.ModelAdmin):
 #2: Fields displayed in Django Admin for the 'MenuLink' model defined in 'models.py'.
 #3: 'Inline' to add/edit 'MenuLink' directly in 'SiteSetup' in Django Admin.
 #4: Settings the 'MenuLink' template to be used as 'inline' in 'SiteSetup'.
-#5: Number of extra entries displayer in the inline interface when adding a new object. 
+#5: Number of extra entries displayer in the inline interface when adding a new object.
+#6: Nomes apresentados como se fossem colunas.
+#7: Se 'return True', poderemos adicionar mais de um usuário em 'Setup'.
+#8: Se este 'return' estiver ativado, vamos poder adicionar apenas um usuário.
