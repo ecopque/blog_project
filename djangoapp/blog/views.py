603c519574399c45a01cb0dc20b0d336c4c8043b
Edson Copque <ecop@disroot.org>
2024-09-10 10:06:02 -0300

    Under construction.

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -3,9 +3,9 @@
 from django.shortcuts import render
 
 # EXPORT⬇: /blog_project/djangoapp/blog/urls.py
-def index(request): ##
+def index(request): #1:
     # IMPORT⬇: /blog_project/djangoapp/templates/blog/pages/index.html
-    return render(request, 'blog/pages/index.html') ##
+    return render(request, 'blog/pages/index.html') #2:
 
 def page(request):
     # IMPORT⬇: /blog_project/djangoapp/templates/blog/pages/page.html
@@ -17,3 +17,6 @@ def post(request):
 
 
 # ------------------------------------------------------------------
+
+#1: Esta linha define uma função chamada index que recebe um parâmetro request. No Django, uma view (como essa) é uma função que recebe um pedido HTTP (request) e retorna uma resposta HTTP. Aqui, a função index será usada para processar o pedido quando o usuário acessa a URL associada a esta view.
+#2: Esta linha utiliza a função render do Django para gerar uma resposta HTTP. A função render pega três parâmetros: o objeto request, o caminho para o template HTML que será renderizado ('blog/pages/index.html'), e opcionalmente um dicionário de contexto que pode ser passado para o template. O template 'blog/pages/index.html' será importado e renderizado como resposta quando a view index for chamada. Esse template está localizado no diretório /blog_project/djangoapp/templates/blog/pages/index.html.
