96c32b03ce9d61ad0fe0d2720c7ee59c07ef24e4
Edson Copque <ecop@disroot.org>
2024-09-10 17:27:04 -0300

    Under construction.

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -7,34 +7,40 @@ posts = list(range(1000)) #4:
 
 # EXPORT⬇: /blog_project/djangoapp/blog/urls.py
 def index(request): #1:
-    paginator = Paginator(posts, 9) ##
-    page_number = request.GET.get("page") ##
-    page_obj = paginator.get_page(page_number) ##
+    paginator = Paginator(posts, 9) #5: #6!:
+    page_number = request.GET.get("page") #7: #9:
+    page_obj = paginator.get_page(page_number) #8:
 
     # IMPORT⬇: /blog_project/djangoapp/templates/blog/pages/index.html
-    return render(request, 'blog/pages/index.html', {'page_obj': page_obj,}) #2: ##
+    return render(request, 'blog/pages/index.html', {'page_obj': page_obj,}) #2: #10:
 
 def page(request):
-    paginator = Paginator(posts, 9) ##
-    page_number = request.GET.get("page") ##
-    page_obj = paginator.get_page(page_number) ##
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     # IMPORT⬇: /blog_project/djangoapp/templates/blog/pages/page.html
-    return render(request, 'blog/pages/page.html', {}) ##
+    return render(request, 'blog/pages/page.html', {}) #11:
 
 def post(request):
-    paginator = Paginator(posts, 9) ##
-    page_number = request.GET.get("page") ##
-    page_obj = paginator.get_page(page_number) ##
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     # IMPORT⬇: /blog_project/djangoapp/templates/blog/pages/post.html
-    return render(request, 'blog/pages/post.html', {}) ##
+    return render(request, 'blog/pages/post.html', {})
 
 
 
 #3: A classe Paginator é usada para dividir um conjunto de dados em páginas menores e gerenciáveis. Neste código, Paginator é utilizado para paginar a lista de posts no blog.
 #4: Define uma lista chamada posts que contém 1000 números (de 0 a 999). Este é um exemplo de lista de objetos que será paginada usando a classe Paginator.
-
+#5: Cria uma instância de Paginator, passando dois argumentos: posts (a lista a ser paginada) e 9 (o número de itens por página). Isso divide a lista de posts em páginas de 9 itens cada.
+#6: Foi dito que o segundo argumento é o número de posts por página. Em meus testes, mudando o número não houve diferença alguma.
+#7: Obtém o número da página da requisição HTTP GET. request.GET.get("page") extrai o parâmetro de query page da URL, que indica qual página deve ser exibida. Se o parâmetro não for passado na URL, o valor será None. Se o parâmetro "page" estiver presente na URL, ele retornará o valor dele; caso contrário, retornará None.
+#8: Utiliza o método get_page() do Paginator para obter o objeto da página correspondente ao número da página (page_number). Se page_number for None ou inválido, ele retornará a primeira página.
+#9: Exemplo: http://127.0.0.1:8000/?page=1.
+#10: O terceiro argumento é um dicionário que representa o contexto a ser passado para o template. page_obj é um objeto retornado pelo método 'paginator.get_page(page_number)'.
+#11: Renderiza o template page.html (importado de /blog_project/djangoapp/templates/blog/pages/page.html) passando um contexto vazio {}.
 
 
 # ------------------------------------------------------------------
