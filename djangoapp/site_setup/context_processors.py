78050538d62d26745e2bf517c224249fca4df7b7
Edson Copque <ecop@disroot.org>
2024-09-11 10:46:12 -0300

    Under construction.

--- djangoapp/site_setup/context_processors.py
+++ djangoapp/site_setup/context_processors.py
@@ -1,6 +1,5 @@
 # FILE: /blog_project/djangoapp/site_setup/context_processors.py
 
-# LOCALE: /blog_project/djangoapp/site_setup/models.py
 from site_setup.models import SiteSetup
 
 def context_processor_example(request):
@@ -8,6 +7,6 @@ def context_processor_example(request):
 
 def site_setup(request):
 
-    # LOCALE: /blog_project/djangoapp/site_setup/models.py:
+    # IMPORT⬇: /blog_project/djangoapp/site_setup/models.py:
     setup = SiteSetup.objects.order_by('-id').first()
     return {'site_setup': 'SITE_SETUP context_processors.py', 'title': 'Title', 'site_setup2': setup,}
