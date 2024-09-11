371c03eccebb25241df79ff631bffe930f4d5f61
Edson Copque <ecop@disroot.org>
2024-09-11 18:06:47 -0300

    Under construction.

--- djangoapp/utils/rands.py
+++ djangoapp/utils/rands.py
@@ -8,12 +8,12 @@ def random_letters(k=5): ##
     return ''.join(SystemRandom().choices(string.ascii_letters + string.digits, k=k)) ##
 
 def slugify_new(text):
-    return slugify(text + random_letters(4))
+    return slugify(text + '-' + random_letters(4))
 
 # print(slugify_new('Vá vô va fi Fi')) #1:
 
 
 
-#1: Resposta: va-vo-va-fi-firh0o;
+#1: Resposta: va-vo-va-fi-fi-syy5;
 
 # https://linktr.ee/edsoncopque
