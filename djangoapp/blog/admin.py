2a521394b2f89b9d61c56a8425c591681cff08e9
Edson Copque <ecop@disroot.org>
2024-09-11 18:54:23 -0300

    Under construction.

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -4,15 +4,15 @@ from django.contrib import admin
 from blog.models import Tag
 
 # IMPORT⬇: /blog/project/djangoapp/blog/models.py
-# URL⬇: http://127.0.0.1:8000/admin/blog/tag/
 @admin.register(Tag) ##
 class TagAdmin(admin.ModelAdmin): ##
+    # URL⬇: http://127.0.0.1:8000/admin/blog/tag/
     list_display = 'id', 'name', 'slug', #2: ##
-    list_display_links = 'name', ##
+    list_display_links = 'id', 'name', ##
     search_fields = 'id', 'name', 'slug', ##
     list_per_page = 10 ##
     ordering = '-id', ##
-    # URL: http://127.0.0.1:8000/admin/blog/tag/add/
+    # URL⬇: http://127.0.0.1:8000/admin/blog/tag/add/
     prepopulated_fields = {"slug": ('name',),} #1:
 
 
