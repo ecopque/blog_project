1354ea87488384588d378a670f452caf2ba3d775
Edson Copque <ecop@disroot.org>
2024-09-11 18:35:27 -0300

    Under construction.

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -3,16 +3,18 @@
 from django.contrib import admin
 from blog.models import Tag
 
+# IMPORT⬇: /blog/project/djangoapp/blog/models.py
+# URL⬇: http://127.0.0.1:8000/admin/blog/tag/
+@admin.register(Tag) ##
+class TagAdmin(admin.ModelAdmin): ##
+    list_display = 'id', 'name', 'slug', ##
+    list_display_links = 'name', ##
+    search_fields = 'id', 'name', 'slug', ##
+    list_per_page = 10 ##
+    ordering = '-id', ##
+    prepopulated_fields = {"slug": ('name',),} #1:
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    list_display = 'id', 'name', 'slug',
-    list_display_links = 'name',
-    search_fields = 'id', 'name', 'slug',
-    list_per_page = 10
-    ordering = '-id',
-    prepopulated_fields = {"slug": ('name,')}
 
-
+#1: Significa que o campo "slug" vai pegar o valor deste outro campo 'name', ou seja, vai permitir na url (admin/blog/tag/add/) quando digitar uma nova tag automaticamente uma nova tag será reescrita.
 
 # https://linktr.ee/edsoncopque
