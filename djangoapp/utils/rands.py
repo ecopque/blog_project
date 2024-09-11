9a0d77e49194d837159b0805fdecdf6a5830ba5d
Edson Copque <ecop@disroot.org>
2024-09-11 17:55:45 -0300

    Under construction.

--- djangoapp/utils/rands.py
+++ djangoapp/utils/rands.py
@@ -1,6 +1,10 @@
 # FILE: /blog_project/djangoapp/utils/rands.py
 
+from random import SystemRandom
+import string
 
+def random_letters(k=5): ##
+    return SystemRandom().choices(string.aschii_letters + string.digits, k=5) ##
 
 
 
