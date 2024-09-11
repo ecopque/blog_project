e3b80a28bade097b4a4e1cd98a676a08a404fd5a
Edson Copque <ecop@disroot.org>
2024-09-11 18:15:33 -0300

    Under construction.

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,9 +1,12 @@
 # FILE: /blog/project/djangoapp/blog/admin.py
 
 from django.contrib import admin
+from blog.models import Tag
 
 
-
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    ...
 
 
 
