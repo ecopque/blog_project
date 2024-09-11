f207233547b5e1e274a0270111ad0bcb7c9da0eb
Edson Copque <ecop@disroot.org>
2024-09-11 10:59:23 -0300

    Under construction.

--- djangoapp/site_setup/context_processors.py
+++ djangoapp/site_setup/context_processors.py
@@ -8,9 +8,9 @@ def context_processor_example(request):
 def site_setup(request):
     # IMPORT⬇: /blog_project/djangoapp/site_setup/models.py:
     setup = SiteSetup.objects.order_by('-id').first() #1: ##
-    return {'site_setup': 'SITE_SETUP context_processors.py', 'title': 'Title', 'site_setup2': setup,} #2: #3:
+    return {'site_setup': 'Hi, context_processor.py', 'title': 'Title', 'site_setup2': setup,} #2: #3:
 
 # ------------------------------------------------------------------
 #1: Recupera o primeiro registro do modelo SiteSetup, ordenado por ID decrescente (o mais recente primeiro).
-#2: No dicionário retornado pela função site_setup, 'site_setup': 'SITE_SETUP context_processors.py' é apenas uma chave chamada 'site_setup' com um valor de string 'SITE_SETUP context_processors.py'. No contexto dos templates, você pode acessar isso usando a variável site_setup.
+#2: No dicionário retornado pela função site_setup, 'site_setup': 'Hi, context_processor.py' é apenas uma chave chamada 'site_setup' com um valor de string 'Hi, context_processor.py'. No contexto dos templates, você pode acessar isso usando a variável site_setup.
 #3: Da mesma forma, 'title': 'Title' é uma chave chamada 'title' com um valor de string 'Title'. Isso pode ser acessado nos templates usando a variável title.
