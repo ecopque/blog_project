a9d04caf5d3d1447a865a0e2e329e105a4dcf004
Edson Copque <ecop@disroot.org>
2024-09-12 14:57:46 -0300

    Under construction.

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,7 +1,7 @@
 # FILE: /blog/project/djangoapp/blog/admin.py
 
 from django.contrib import admin
-from blog.models import Tag
+from blog.models import Tag, Category
 
 # IMPORT⬇: /blog/project/djangoapp/blog/models.py
 @admin.register(Tag) #3:
@@ -14,6 +14,15 @@ class TagAdmin(admin.ModelAdmin): #4:
     ordering = '-id', #8:
     # URL⬇: http://127.0.0.1:8000/admin/blog/tag/add/
     prepopulated_fields = {"slug": ('name',),} #1:
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug',
+    list_display_links = 'name',
+    search_fields = 'id', 'name', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {"slug": ('name',),}
 
 
 
