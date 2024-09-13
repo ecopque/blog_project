bc2c3ace1fd8a8cacc57ad27836e32200c80df54
Edson Copque <ecop@disroot.org>
2024-09-12 21:14:37 -0300

    Under construction.

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -67,11 +67,17 @@ class Post(models.Model):
     #EXPORT⬇: /blog_project/data/web/media/posts/year/month/
     cover = models.ImageField(upload_to='post/%Y/%m/', blank=True, default='') #14: ##
     cover_in_post_content = models.BooleanField(default=True, help_text='Display the cover image also within the post content?') #15: ##
+    created_at = models.DateTimeField(auto_now_add=True) #16: ##
+    updated_at = models.DateTimeField(auto_now=True) #17: ##
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,) #18: ##
     
 
 #13: Este camarada será o resumo do nosso post.
 #14: Este aqui será a capa do post.
 #15: Se marcado, vai exibir a imagem de capa dentro do post.
+#16: Este é o campo de data do post. Quando o post for salvo ele vai adicionar a data ao post.
+#17: Toda vez que salvar o post vai ser gerado uma nova data.
+#18: 
 
 # ------------------------------------------------------------------
 #7: Após criar o 'Category()' e 'CategoryAdmin()' você precisa realizar o makemigrations. Melhor rodar o comando 'docker compose up --build --force-recreate'.
