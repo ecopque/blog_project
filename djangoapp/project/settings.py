51bf0ad87040be54eb01f7a5371795a97f9bfda9
Edson Copque <ecop@disroot.org>
2024-09-14 11:04:46 -0300

    Configuring django-summernote as html editor in textfield in models

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'blog',
     # IMPORT⬇: /blog_project/djangoapp/site_setup/apps.py
     'site_setup',
-    'django_summernote'
+    'django_summernote' ##
 ]
 
 MIDDLEWARE = [
