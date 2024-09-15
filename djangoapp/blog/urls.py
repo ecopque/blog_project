2638eba064f6d7dda5ab78f226e9d97919b60db7
Edson Copque <ecop@disroot.org>
2024-09-15 18:13:35 -0300

    Filtering and sorting posts sent to the view template

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
 
 
 # ------------------------------------------------------------------
-
 #1: Esta linha define o namespace do aplicativo para o qual as URLs estão sendo configuradas. O Django permite agrupar as URLs de um aplicativo sob um namespace para evitar conflitos de nome com outras aplicações dentro do mesmo projeto. Nesse caso, o app_name é definido como 'blog', o que significa que todas as URLs definidas nesse arquivo serão referenciadas com o prefixo blog:. Por exemplo, a URL nomeada index será referenciada como blog:index.
 #2: Esta linha define uma rota no Django. Aqui, a função path define o mapeamento entre a URL e a view correspondente. Esta rota é para a URL raiz (''), que significa que quando o usuário acessa a URL principal do aplicativo (sem nenhum caminho adicional), a view index será chamada. O argumento name='index' atribui um nome a essa URL, permitindo que ela seja referenciada de forma reversa no código e nos templates usando blog:index (devido ao app_name definido anteriormente).
