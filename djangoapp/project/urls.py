bd938b09787b124f8bd8fb39b6cb47433916cae4
Edson Copque <ecop@disroot.org>
2024-09-15 10:48:10 -0300

    Configuring django-summernote as html editor in textfield in models

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('summernote/', include('django_summernote.urls')), ##
+    path('summernote/', include('django_summernote.urls')), #1:
 ]
 
 if settings.DEBUG:
@@ -17,3 +17,4 @@ if settings.DEBUG:
 
 
 # ------------------------------------------------------------------
+#1: Esta linha adiciona as URLs do pacote django-summernote ao projeto Django. Quando a URL /summernote/ for acessada, as URLs e as views fornecidas pelo django_summernote serão carregadas, permitindo a utilização do editor Summernote no site. Ela se conecta ao pacote django_summernote para manipular e exibir o editor de texto.
