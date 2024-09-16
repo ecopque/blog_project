a06443497ffe5d593cff97dc59d0e68318ab2039
Edson Copque <ecop@disroot.org>
2024-09-16 10:54:12 -0300

    get_absolute_url and creating a readonly field in django admin

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -3,8 +3,8 @@
 from django.contrib import admin
 from blog.models import Tag, Category, Page, Post
 from django_summernote.admin import SummernoteModelAdmin #23:
-from django.urls import reverse ##
-from django.utils.safestring import mark_safe ##
+from django.urls import reverse #29:
+from django.utils.safestring import mark_safe #30:
 
 # IMPORT⬇: /blog/project/djangoapp/blog/models.py
 @admin.register(Tag) #3:
@@ -63,17 +63,17 @@ class PostAdmin(SummernoteModelAdmin): #26:
     list_editable = 'is_published', #12:
     ordering = '-id',
     # URL⬇: http://127.0.0.1:8000/admin/blog/post/add/
-    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by', 'link', #13:
+    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by', 'link', #13: #31:
     prepopulated_fields = {"slug": ('title',),} #14:
     autocomplete_fields = 'tags', 'category' #15:
 
-    def link(self, obj):
+    def link(self, obj): #31:
         if not obj.pk:
             return '-'
         
         # IMPORT⬇: /blog_project/djangoapp/blog/urls.py
-        url_post = reverse('blog:post', args=(obj.slug,)) #27: ##
-        safe_link = mark_safe(f'<a target="_blank" href="{url_post}">View post</a>') #28: ##
+        url_post = reverse('blog:post', args=(obj.slug,)) #27: #32:
+        safe_link = mark_safe(f'<a target="_blank" href="{url_post}">View post</a>') #28: #33:
         return  safe_link
 
     # IMPORT⬇: /blog/project/djangoapp/blog/models.py
@@ -88,6 +88,11 @@ class PostAdmin(SummernoteModelAdmin): #26:
 
 #27: BLOG: é a namespace da URL definida nas rotas (URLs) do Django. Ele faz parte do reverse('blog:post', ...), que indica que existe um app chamado 'blog' e que dentro dele há uma URL com o nome 'post'. POST: refere-se ao nome de uma rota específica dentro do app 'blog'. Geralmente, este nome de rota aponta para a view que renderiza uma página de um post específico. SLUG: faz parte da URL do post. No código args=(obj.slug,), o slug é passado como argumento, o que indica que ele é um identificador único utilizado para formar a URL de um post específico. O slug é geralmente uma string legível, derivada do título do post, usada para criar URLs amigáveis.
 #28: Quando você for renderizar html no Django, você precisará informar que é seguro, ou seja, usar 'mark_safe'.
+#29: O reverse é utilizado para gerar URLs dinâmicas baseadas em nomes de rotas. Neste código, ele será usado para construir o link para visualizar um post diretamente da página de administração.
+#30: é uma função usada para marcar uma string como "segura" para renderização em HTML, ou seja, ela impede que o Django escape o HTML contido na string. Nesse caso, será utilizado para gerar um link HTML clicável no painel de administração.
+#31: O 'link' dentro da variável é entendido como a função 'link', logo abaixo.
+#32: Esta linha está utilizando a função reverse para gerar a URL de um post específico. O nome da rota 'blog:post' está sendo usado junto com o slug do objeto (obj) como argumento. O slug serve como identificador único do post na URL.
+#33: Ele cria um link para o post gerado pela URL url_post e abre-o em uma nova aba (target="_blank").
 # ------------------------------------------------------------------
 #22: Este é o 'contente' do Post() de /blog/project/djangoapp/blog/models.py.
 #23: Importa a classe SummernoteModelAdmin do pacote django_summernote, que é uma extensão da classe ModelAdmin do Django. Essa classe facilita a integração do editor Summernote nos campos de texto dos modelos administrados.
