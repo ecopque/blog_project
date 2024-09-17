9c4ddfae6b8b68d82ed72d389d71901aa6eed81e
Edson Copque <ecop@disroot.org>
2024-09-17 07:59:47 -0300

    Starting the internal part of the post

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -30,13 +30,13 @@ def page(request):
     return render(request, 'blog/pages/page.html', {}) #11:
 
 def post(request, slug): #14:
-    post = Post.objects.get_published().filter(slug=slug).first() ##
+    post = (Post.objects.get_published().filter(slug=slug).first()) ##
     # paginator = Paginator(posts, 9)
     # page_number = request.GET.get("page")
     # page_obj = paginator.get_page(page_number)
 
     # IMPORT⬇: /blog_project/djangoapp/templates/blog/pages/post.html
-    return render(request, 'blog/pages/post.html', {'post', post,}) ##
+    return render(request, 'blog/pages/post.html', {'post':post,}) ##
 
 
 
