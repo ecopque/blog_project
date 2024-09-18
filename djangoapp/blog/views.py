5840614320b001b765d89431bc5a06dab81cadab
Edson Copque <ecop@disroot.org>
2024-09-18 14:54:59 -0300

    Creating page titles and using http404 for pages not found

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -29,26 +29,18 @@ def index(request): #1:
 #     # IMPORT⬇: /blog_project/djangoapp/templates/blog/pages/page.html
 #     return render(request, 'blog/pages/page.html', {}) #11:
 
-def post(request, slug): #14:
-    post = (Post.objects.get_published().filter(slug=slug).first()) #16:
-    # paginator = Paginator(posts, 9)
-    # page_number = request.GET.get("page")
-    # page_obj = paginator.get_page(page_number)
-    # IMPORT⬇: /blog_project/djangoapp/templates/blog/pages/post.html
-    return render(request, 'blog/pages/post.html', {'post':post,}) #17:
-
 def created_by(request, author_pk):
     user = User.objects.filter(pk=author_pk).first() ##
     if user is None: ##
         raise Http404() ##
-
+    
     posts = Post.objects.get_published().filter(created_by__pk=author_pk) #18:
     
     user_full_name = user.username ##
     if user.first_name:
         user_full_name = f'{user.first_name} {user.last_name}' ##
+    
     page_title = 'Posts by ' + user_full_name + ' - ' ##
-
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -63,9 +55,7 @@ def category(request, slug):
 
     if len(posts) == 0: ##
         raise Http404() ##
-
     page_title = f'{page_obj[0].category.name} Category - ' ##
-
     return render(request, 'blog/pages/index.html', {'page_obj':page_obj, 'page_title': page_title,}) ##
 
 def tag(request, slug):
@@ -74,17 +64,31 @@ def tag(request, slug):
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-    return render(request, 'blog/pages/index.html', {'page_obj':page_obj,})
+
+    if len(page_obj) == 0:
+        raise Http404
+    page_title = f'{page_obj[0].tags.first().name} - Tags - ' ##
+    return render(request, 'blog/pages/index.html', {'page_obj':page_obj, 'page_title':page_title,}) ##
 
 def search(request):
     # IMPORT⬇: /blog/project/djangoapp/blog/models.py
     search_value = request.GET.get('search', '').strip() #22:
     posts = Post.objects.get_published().filter(Q(title__icontains=search_value) | Q(excerpt__icontains=search_value) | Q(content__icontains=search_value))[0:PER_PAGE] #23:
-    return render(request, 'blog/pages/index.html', {'page_obj':posts, 'search_value':search_value,}) #24:
+
+    page_title = f'{search_value[:30]} - Search - ' ##
+    return render(request, 'blog/pages/index.html', {'page_obj':posts, 'search_value':search_value, 'page_title':page_title,}) #24: ##
 
 def page(request, slug):
     page = (Page.objects.filter(is_published=True).filter(slug=slug).first())
     return render(request,'blog/pages/page.html',{'page': page,}) #25:
+
+def post(request, slug): #14:
+    post = (Post.objects.get_published().filter(slug=slug).first()) #16:
+    # paginator = Paginator(posts, 9)
+    # page_number = request.GET.get("page")
+    # page_obj = paginator.get_page(page_number)
+    # IMPORT⬇: /blog_project/djangoapp/templates/blog/pages/post.html
+    return render(request, 'blog/pages/post.html', {'post':post,}) #17:
 
 # ------------------------------------------------------------------
 #25: Essa linha faz o render da página usando o template page.html, passando o objeto page para o contexto. O template page.html vai exibir os dados da página (como title e content), usando o objeto page.
